fd124a18148e5a1dbaa69838ca60b6f35045b6ce
CrunchyDuck <realvulpes@gmail.com>
2026-02-15 01:50:43 +0000

    Initial commit

---
+++ archive_bot.py
@@ -0,0 +1,107 @@
+import discord
+import re
+import datetime
+from pathlib import Path
+
+# TODO: Choose file to load.
+# TODO: Fetch colour of user, or fall back to a palette.
+
+with open("token.txt", "r") as f:
+	TOKEN = f.readline()
+
+
+class MyClient(discord.Client):
+	async def on_ready(self):
+		print('Logged on as', self.user)
+
+	async def on_message(self, message):
+		# only respond to ourselves
+		if message.author != self.user:
+			return
+
+		c = message.content
+		if c.startswith("P.archive"):
+			channel_id = re.search(r'id=(\d+)', c)
+			if channel_id is None:
+				print("Couldn't find channel ID in message" + c)
+				return
+			print(f"Getting channel with id {channel_id.group(1)}...")
+			channel = self.get_channel(int(channel_id.group(1)))
+			if channel is None:
+				print("Couldn't find channel with ID " + channel_id.group(1))
+				return
+			await self.archive_dm_history(channel)
+
+	async def archive_dm_history(self, channel):
+		grouping_limit = datetime.timedelta(minutes=10)  # A threshold for the range of time messages will be grouped for.
+		last_author = ""
+		last_date = ""  # The d/m/y of the last message.
+		last_datetime = None
+
+		milestones_every = datetime.timedelta(seconds=30)
+		last_update = datetime.datetime.now()
+		archive_count = 0
+
+		print(f"Archiving channel {channel.id}...")
+		f = open(f"{channel.id}.html", "w", encoding="utf-8")
+		f.write("""<!DOCTYPE html>
+		<html>
+		<head>
+		</head>""")
+		f.write('<body><p>')
+		async for message in channel.history(limit=None, oldest_first=True):
+			archive_count += 1
+			if datetime.datetime.now() - last_update >= milestones_every:
+				print(f"Archived {archive_count} messages.")
+				last_update = datetime.datetime.now()
+			m_t = message.created_at  # Datetime object
+			m_date = m_t.strftime("%d/%m/%Y")  # Date as a string in my preferred format.
+			m_time = m_t.strftime("%H:%M:%S")  # Time as a string in my preferred format.
+			time_diff = 0
+			if last_datetime is not None:
+				time_diff = m_t - last_datetime
+
+			author = str(message.author.display_name)
+			msg_format = f'<span><span class="time">{m_time}</span> {message.content}</span>'  # How a message is displayed in my formatting.
+
+			# If a new day has started, cause a message break.
+			if m_date != last_date:
+				f.write(f'\n<span class="date">======== {m_date} ========</span>\n')
+			if last_author != author or time_diff >= grouping_limit or m_date != last_date:
+				cls = "otherAuthor"
+				if message.author == self.user:
+					cls = "botAuthor"
+				f.write(f'<span class="{cls}">{author}</span>\n')
+			f.write(f"{msg_format}\n")
+
+			# Save attached files
+			if message.attachments:
+				atts_string = ""
+				atts_tags_string = ""
+				for att in message.attachments:
+					try:
+						atts_string += f"({att.id}_{att.filename}) "
+						fname = f"{att.id}_{att.filename}"
+						p = Path("attachments", str(channel.id), fname)
+						p.parent.mkdir(parents=True, exist_ok=True)
+						with open(p, "w+"):
+							pass
+						await att.save(p)
+						if re.search(r".(png|jpg|gif|jpeg)$", att.filename) is not None:
+							atts_tags_string += f"<img src='attachments/{channel.id}/{fname}'>\n"
+					except (discord.NotFound, discord.HTTPException) as e:
+						print(f"==Attachment Error==\nMessage snowflake: {message.id}\nError: {e}")
+				f.write(f"<span>File: {atts_string}</span>\n")
+				if atts_tags_string:
+					f.write(f"<span>{atts_tags_string}</span>\n")
+
+			last_date = m_date
+			last_datetime = m_t
+			last_author = author
+		f.write("</p></body></html>")
+		f.close()
+		print("Finished archiving channel: " + str(channel.id))
+
+
+client = MyClient()
+client.run(TOKEN)
---
+++ archive_viewer.js
@@ -0,0 +1,112 @@
+const text_indent_amount = 15;
+loaded_files = {}
+const parse_html_archive = /(.*?)<p>/gs;
+const archive_clip_from_end = "</body></html>"
+
+window.onload = function() {
+    add_archive_directory_listener()
+}
+
+function add_archive_directory_listener() {
+    var fileSelector = document.getElementById('archive_folder_picker');
+    fileSelector.addEventListener('change', on_select_archive_directory)
+}
+
+function on_select_archive_directory(event) {
+    var channel_selector = document.getElementById("channel_selector");
+
+    // Remove old structure
+    channel_selector.innerHTML = "";
+
+    var folder_structure = create_folder_structure(event.target.files);
+    var root_folder = Object.keys(folder_structure)[0];
+    read_archive_folder(folder_structure[root_folder], root_folder, root_folder, channel_selector, 0);
+}
+
+function create_folder_structure(files) {
+    // Takes the list of files and their paths, and creates a json object of the structure.
+    var folder_structure = {};
+    for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+        loaded_files[file.webkitRelativePath] = file;
+        let paths = file.webkitRelativePath.split("/");
+        folder_pos = folder_structure;
+        for (let j = 0; j < paths.length; j++) {
+            let path = paths[j];
+            // Folder
+            if (j != paths.length - 1) {
+                if (!(path in folder_pos)) {
+                    folder_pos[path] = {};
+                }
+                folder_pos = folder_pos[path];
+            }
+            // File
+            else {
+                if (!("!!files" in folder_pos)) {
+                    folder_pos["!!files"] = [];
+                }
+                folder_pos["!!files"].push(path);
+            }
+        }
+    }
+
+    return folder_structure;
+}
+
+function read_archive_folder(folder_contents, folder_name, current_path, parent_element, indent_level) {
+    var my_details = create_dropdown(folder_name, indent_level);
+    parent_element.append(my_details);
+
+    for (var entry in folder_contents) {
+        if (entry == "!!files") {
+            continue;
+        }
+        read_archive_folder(folder_contents[entry], entry, current_path + `/${entry}`, my_details, indent_level + 1);
+    }
+
+    if (!("!!files" in folder_contents))
+        return;
+    for (var file_name of folder_contents["!!files"]) {
+        var extension = file_name.substring(file_name.length - 4)
+        if (extension != "html")
+            continue;
+        my_details.append(create_archive_selector(file_name, current_path + `/${file_name}`, indent_level + 1));
+    }
+}
+
+function create_dropdown(label, indent_level) {
+    // Add relevant HTML to element; Return new container
+    var details = document.createElement("details");
+    details.classList.add("details_dropdown");
+    details.classList.add("text");
+    var indent = text_indent_amount * indent_level;
+    details.innerHTML = `<summary style="text-indent: ${indent}px;">${label}</summary>`
+    return details;
+}
+
+function create_archive_selector(label, file_path, indent_level) {
+    // Add clickable loading thingy.
+    console.log(file_path);
+    var text_div = document.createElement("div");
+    text_div.classList.add("archive_selector");
+    var indent = text_indent_amount * indent_level;
+    text_div.style.textIndent = `${indent}px`;
+    text_div.innerHTML = `<span data-file_path="${file_path}" onclick="clicked_archive(this);">#${label}</span>`
+    return text_div;
+}
+
+// From: https://stackoverflow.com/a/196510
+function clicked_archive(element) {
+    document.getElementById("archive_content").innerHTML = "";
+    var element_path = element.getAttribute("data-file_path");
+    var archive_file = loaded_files[element_path];
+    var reader = new FileReader();
+    reader.onload = () => {
+        loaded_archive(reader.result)
+    };
+    reader.readAsText(archive_file);
+}
+
+function loaded_archive(archive_content) {
+    document.getElementById("archive_content").innerHTML = archive_content;
+}
